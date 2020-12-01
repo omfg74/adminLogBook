0ee8b1cac8cc25f33b52ba11302e495851065b5d
Aleksandr Podsivalov <podsh74@gmail.com>
2020-12-01 21:16:58 +0500

    Main refactoring

--- src/main/java/com/omfgdevelop/jwtdemo/AdminLogBookApplication.java
+++ src/main/java/com/omfgdevelop/jwtdemo/AdminLogBookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtdemoApplication {
+public class AdminLogBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtdemoApplication.class, args);
+		SpringApplication.run(AdminLogBookApplication.class, args);
 	}
 
 }
